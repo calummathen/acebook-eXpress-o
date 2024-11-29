0cbe2aec8151c2c3e99a77e12bef51db9be25a1d
Calum Mathen <calummathen@hotmail.co.uk>
2024-11-29 14:03:59 +0000

    Added friends request for Another User

--- api/routes/friends.js
+++ api/routes/friends.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const FriendsController = require("../controllers/friends");
 
 router.get("/", FriendsController.getFriendsForUser);
+router.get("/:username", FriendsController.getFriendsForAnotherUser);
 
 module.exports = router;
