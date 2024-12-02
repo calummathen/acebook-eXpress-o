558ef8a4c0f7ccc9a28c5ddefd7f76c7f4a5eb25
Jamie King <jamiedanielking@gmail.com>
2024-12-02 13:34:47 +0000

    Remove duplicate friends

--- api/routes/friends.js
+++ api/routes/friends.js
@@ -7,6 +7,5 @@ router.get("/", FriendsController.getFriendsForUser);
 router.get("/requests", FriendsController.getUnapprovedFriendsForUser);
 router.get("/:username", FriendsController.getFriendsForAnotherUser);
 router.post("/request", FriendsController.sendFriendRequest);
-router.get("/:username", FriendsController.getFriendsForAnotherUser);
 
 module.exports = router;
