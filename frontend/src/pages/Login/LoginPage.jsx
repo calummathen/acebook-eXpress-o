5876aeef73b54422adf3b63d584d100b7785e23e
Jamie King <jamiedanielking@gmail.com>
2024-12-03 11:55:25 +0000

    Refactored login/signup frontend files to look nicer

--- frontend/src/pages/Login/LoginPage.jsx
+++ frontend/src/pages/Login/LoginPage.jsx
@@ -1,67 +1,89 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
-import { login } from "../../services/authentication";
-
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+import { login } from "../../services/authentication";
 
 import "./LoginPage.css";
 
 export function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    try {
-      const token = await login(email, password);
-      localStorage.setItem("token", token);
-      navigate("/posts");
-    } catch (err) {
-      console.error(err);
-      toast.error("Login failed. Please check your credentials.")
-      navigate("/");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    const navigate = useNavigate();
+    
+    async function handleSubmit(event) {
+
+        event.preventDefault();
+
+        if (email == "" && password == "") {
+            toast.error("Please enter your credentials");
+            return;
+        } else if (email == "") {
+            toast.error("Please enter your email");
+            return;
+        } else if (password == "") {
+            toast.error("Please enter your password");
+            return;
+        }
+
+        try {
+
+            const token = await login(email, password);
+            localStorage.setItem("token", token);
+            navigate("/posts");
+
+        } catch (err) {
+
+            console.error(err);
+            toast.error("Login failed. Please check your credentials.")
+            setPassword("");
+
+        }
     }
-  }
+    
+    return (
+        <div className="wrapper-auth">
 
-  function handleEmailChange(event) {
-    setEmail(event.target.value);
-  }
+            <ToastContainer toastStyle={{ backgroundColor: "#E4E0E1", color: "#493628" }} />
+    
+            <div className="logo-auth">
+                <h1>BeanScene</h1>
+                <p>Stirring Up Your Social Life</p>
+            </div>
 
-  function handlePasswordChange(event) {
-    setPassword(event.target.value);
-  }
+            <div className="box-auth">
+                <h2>Login</h2>
 
-  return (
-    <div className="wrapper-auth">
-      <ToastContainer toastStyle={{ backgroundColor: "#E4E0E1", color: "#493628" }} />
-      <div className="logo-auth">
-        <h1>BeanScene</h1>
-        <p>Stirring Up Your Social Life</p>
-      </div>
-      <div className="box-auth">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit} className="login">
-          <label htmlFor="email">Email:</label>
-          <input
-            id="email"
-            type="text"
-            value={email}
-            onChange={handleEmailChange}
-          />
-          <label htmlFor="password">Password:</label>
-          <input
-            id="password"
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-          <Link id="signup" to="/signup">Sign Up</Link>
-          <input role="submit-button" id="submit" type="submit" value="Login" />
-        </form>
-      </div>
-    </div>
-  );
+                <form onSubmit={handleSubmit} className="login">
+
+                    <label id="emailLabel" htmlFor="email">Email:</label>
+                    <input
+                        id="email"
+                        type="text"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <label id="passwordLabel" htmlFor="password">Password:</label>
+                    <input
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    
+                    <Link id="signup" to="/signup">Sign Up</Link>
+                    <input
+                        id="submit"
+                        type="submit"
+                        value="Login"
+                        role="submit-button"
+                    />
+                </form>
+            </div>
+        </div>
+    );
 }
--- frontend/src/pages/Signup/SignupPage.jsx
+++ frontend/src/pages/Signup/SignupPage.jsx
@@ -1,197 +1,207 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
-import { signup } from "../../services/authentication";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+import { signup } from "../../services/authentication";
 
 import "./SignupPage.css";
 
 export function SignupPage() {
-  const [name, setName] = useState("");
-  const [birthday, setBirthday] = useState("");
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const navigate = useNavigate();
 
-  const validatePassword = (password) => {
-    const chars = /[!#$%&'()*+,\-:;<=>?@£÷]/;
+    const [ name, setName ] = useState("");
+    const [ birthday, setBirthday ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ username, setUsername ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ confirmPassword, setConfirmPassword ] = useState("");
+
+    const navigate = useNavigate();
     
-    if (password == "") {
-      toast.error("Please enter a password")
-    } else if (password !== confirmPassword) {
-      toast.error("Passwords must match")
-    } else if (password.length < 8) {
-      toast.error("Password must be 8 or more characters long");
-    } else if (!chars.test(password)) {
-      toast.error("Must contain at least 1 special character");
-    } else {
-      return true;
-    }
-  };
-
-  const validateUsername = (username) => {
-    if (!username) {
-      toast.error("Input Username.");
-      return false;
-    }
-    return true;
-  };
-
-  const validateEmail = (email) => {
-    // Check if email contains "@" and "."
-    // "||" MEANS "OR"
-    if (!email || !email.includes("@") || !email.includes(".")) {
-      toast.error("Please enter a valid email address with '@' and '.'");
-      return false;
-    }
-    return true;
-  };
-
-  const validateBirthday = (birthday) => {
-    if (!birthday) {
-      toast.error("Birthday is required.");
-      return false;
-    }
-    return true;
-  };
-
-  const validateName = (name) => {
-    if (!name) {
-      toast.error("Input Name.");
-      return false;
-    }
-    return true;
-  };
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-    if (
-      validatePassword(password) &&
-      validateUsername(username) &&
-      validateEmail(email) &&
-      validateBirthday(birthday) &&
-      validateName(name)
-    ) {
-      try {
-        await signup(name, birthday, email, username, password);
-        navigate("/login");
-      } catch (err) {
-        console.error(err);
-        const errorMessage = err.message;
-
-        if (errorMessage === "username") {
-          toast.error("That username is taken, please try something else.");
-        } else if (errorMessage === "email") {
-          toast.error(
-            "An account with that email already exists, please login."
-          );
-          // } else if (errorMessage === "password") {
-          //   toast.error("Password required");
-          // } else if (errorMessage === "birthday") {
-          //   toast.error("Birthday required");
-          // } else if (errorMessage === "name") {
-          //   toast.error("Missing Name");
+    const validatePassword = (password) => {
+        const chars = /[!#$%&'()*+,\-:;<=>?@£÷]/;
+        
+        if (password == "") {
+            toast.error("Please enter a password")
+        } else if (password !== confirmPassword) {
+            toast.error("Passwords must match")
+        } else if (password.length < 8) {
+            toast.error("Password must be 8 or more characters long");
+        } else if (!chars.test(password)) {
+            toast.error("Password must contain at least 1 special character");
         } else {
-          toast.error("An unexpected error occurred. Please try again.");
+            return true;
         }
-        navigate("/signup");
-      }
+    };
+    
+    const validateUsername = (username) => {
+
+        if (!username) {
+            toast.error("Please enter a username");
+            return false;
+        }
+
+        return true;
+    };
+    
+    const validateEmail = (email) => {
+
+        // Check if email contains "@" and "."
+        // "||" MEANS "OR"
+
+        if (!email || !email.includes("@") || !email.includes(".")) {
+            toast.error("Please enter a valid email address with an '@' and '.'");
+            return false;
+        }
+
+        return true;
+    };
+    
+    const validateBirthday = (birthday) => {
+
+        if (!birthday) {
+            toast.error("Please enter your birthday");
+            return false;
+        }
+
+        return true;
+    };
+    
+    const validateName = (name) => {
+
+        if (!name) {
+            toast.error("Please enter your name");
+            return false;
+        }
+
+        return true;
+    };
+    
+    async function handleSubmit(event) {
+
+        event.preventDefault();
+
+        if (
+            validatePassword(password) &&
+            validateUsername(username) &&
+            validateEmail(email) &&
+            validateBirthday(birthday) &&
+            validateName(name)
+        ) {
+
+            try {
+
+                await signup(name, birthday, email, username, password);
+                navigate("/");
+
+            } catch (err) {
+
+                console.error(err);
+                const errorMessage = err.message;
+                
+                if (errorMessage === "username") {
+
+                    toast.error("That username is taken, please try something else");
+
+                } else if (errorMessage === "email") {
+
+                    toast.error(
+                        "An account with that email already exists, please login."
+                    );
+
+                } else {
+
+                    toast.error("An unexpected error occurred. Please try again.");
+
+                }
+
+                setPassword("");
+            }
+        }
     }
-  }
+    
+    const date18YearsAgo = new Date();
+    date18YearsAgo.setFullYear(date18YearsAgo.getFullYear() - 18);
+    
+    const formattedDate = `${date18YearsAgo.getFullYear()}-${
+        (date18YearsAgo.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")
+    }-${date18YearsAgo.getDate().toString().padStart(2, "0")}`;
+    
+    return (
+        <div className="wrapper-auth">
 
-  function handleNameChange(event) {
-    setName(event.target.value);
-  }
+            <ToastContainer toastStyle={{ backgroundColor: "#E4E0E1", color: "#493628" }} />
 
-  function handleBirthdayChange(event) {
-    setBirthday(event.target.value);
-  }
+            <div className="logo-auth">
+                <h1>BeanScene</h1>
+                <p>Stirring Up Your Social Life</p>
+            </div>
 
-  function handleEmailChange(event) {
-    setEmail(event.target.value);
-  }
+            <div className="box-auth">
+                <h2>Signup</h2>
+            
+                <form onSubmit={handleSubmit} className="signup">
+            
+                    <label id="usernameLabel" htmlFor="username">Username:</label>
+                    <input
+                        id="username"
+                        type="text"
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
+            
+                    <label id="emailLabel" htmlFor="email">Email:</label>
+                    <input
+                        id="email"
+                        type="text"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+        
+                    <label id="nameLabel" htmlFor="name">Name:</label>
+                    <input
+                        id="name"
+                        type="text"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+        
+                    <label id="birthdayLabel" htmlFor="birthday">Birthday:</label>
+                    <input
+                        id="birthday"
+                        type="date"
+                        value={birthday}
+                        max={formattedDate}
+                        onChange={e => setBirthday(e.target.value)}
+                    />
 
-  function handleUsernameChange(event) {
-    setUsername(event.target.value);
-  }
+                    <label id="passwordLabel" htmlFor="password">Password:</label>
+                    <input
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
 
-  function handlePasswordChange(event) {
-    setPassword(event.target.value);
-  }
+                    <label id="confirmPasswordLabel" htmlFor="password">Confirm Password:</label>
+                    <input
+                        id="confirmPassword"
+                        type="password"
+                        value={confirmPassword}
+                        onChange={e => setConfirmPassword(e.target.value)}
+                    />
 
-  function handleConfirmPasswordChange(event) {
-    setConfirmPassword(event.target.value);
-  }
-
-  const date18YearsAgo = new Date();
-  date18YearsAgo.setFullYear(date18YearsAgo.getFullYear() - 18);
-
-  const formattedDate = `${date18YearsAgo.getFullYear()}-${(
-    date18YearsAgo.getMonth() + 1
-  )
-    .toString()
-    .padStart(2, "0")}-${date18YearsAgo.getDate().toString().padStart(2, "0")}`;
-
-  return (
-    <div className="wrapper-auth">
-      <ToastContainer />
-      <div className="logo-auth">
-        <h1>BeanScene</h1>
-        <p>Stirring Up Your Social Life</p>
-      </div>
-      <div className="box-auth">
-        <h2>Signup</h2>
-        <form onSubmit={handleSubmit} className="signup">
-          <label id="usernameLabel" htmlFor="username">Username:</label>
-          <input
-            id="username"
-            type="text"
-            value={username}
-            onChange={handleUsernameChange}
-          ></input>
-          <label id="emailLabel" htmlFor="email">Email:</label>
-          <input
-            id="email"
-            type="text"
-            value={email}
-            onChange={handleEmailChange}
-          />
-          <label id="nameLabel" htmlFor="name">Name:</label>
-          <input
-            id="name"
-            type="text"
-            value={name}
-            onChange={handleNameChange}
-          ></input>
-          <label id="birthdayLabel" htmlFor="birthday">Birthday:</label>
-          <input
-            id="birthday"
-            type="date"
-            value={birthday}
-            max={formattedDate}
-            onChange={handleBirthdayChange}
-          ></input>
-          <label id="passwordLabel" htmlFor="password">Password:</label>
-          <input
-            id="password"
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-          <label id="confirmPasswordLabel" htmlFor="password">Confirm Password:</label>
-          <input
-            id="confirmPassword"
-            type="password"
-            value={confirmPassword}
-            onChange={handleConfirmPasswordChange}
-          />
-          <Link id="login" to="/">Login</Link>
-          <input role="submit-button" id="submit" type="submit" value="Sign Up" />
-        </form>
-      </div>
-    </div>
-  );
+                    <Link id="login" to="/">Login</Link>
+                    <input
+                        role="submit-button"
+                        id="submit"
+                        type="submit"
+                        value="Sign Up"
+                    />
+                </form>
+            </div>
+        </div>
+    );
 }
