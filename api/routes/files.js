9e25bbe03165f099e9ab67d03ea69f748e2047a9
altota90 <111538860+altota90@users.noreply.github.com>
2024-12-04 15:28:21 +0000

    Update files.js
    POST route fixed

--- api/routes/files.js
+++ api/routes/files.js
@@ -3,7 +3,7 @@ const { uploadFile, getFile } = require("../controllers/files");
 
 const router = express.Router();
 
-router.post("/files/upload", uploadFile); // Route for uploading files
+router.post("/upload", uploadFile); // Route for uploading files
 router.get("/files", getFile); // Route for retrieving files
 
 module.exports = router;
