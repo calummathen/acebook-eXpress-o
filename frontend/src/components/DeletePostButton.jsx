7059cd13a9c1e051f8449268e8047520fd1a11d1
Jamie King <jamiedanielking@gmail.com>
2024-12-05 10:21:26 +0000

    Finished main post bits

--- frontend/src/components/DeletePostButton.jsx
+++ frontend/src/components/DeletePostButton.jsx
@@ -1,24 +1,26 @@
 import { useConfirm } from "material-ui-confirm";
 import { FaRegTrashCan } from "react-icons/fa6";
 
-const DeletePostId = ({post_id, DeletePostId, UpdatePost, isYours, editState}) =>{
+const DeletePostId = ({post_id, DeletePostId, UpdatePost, isYours, editState}) => {
 
     const confirm = useConfirm()
     
     const token = localStorage.getItem("token");
     
     return (
-        <button onClick={() => {
+        <button className="post-delete-button" onClick={() => {
+
             confirm({ description: "Post will be permanently deleted!" })
-            .then(() => {
-                console.log("Awawa")
-                DeletePostId(token, post_id, isYours)
-                setTimeout(() => {UpdatePost(Math.random())}, 200)
-            })
-            .catch(() => {
-                console.log("Post delete cancelled")
-            })
-        }}>Delete Post</button>
+                .then(() => {
+                    DeletePostId(token, post_id, isYours)
+                    setTimeout(() => {UpdatePost(Math.random())}, 200)
+                })
+                .catch(() => {
+                    console.log("Post delete cancelled")
+                })
+        }} disabled={editState}>
+            <FaRegTrashCan />
+        </button>
     )
     
 };
